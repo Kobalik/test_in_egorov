e0ccee86f96c763a2aa15bc3fa3c1304c3609f74
Valentin Koval <Ldidi1@yandex.ru>
2022-06-30 19:08:21 +0300

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,2 +1,2 @@
 <?php   
-    $connect = new mysqli('localhost', 'root', '', 'test_in_egorov');
+    $connect = new mysqli('sql203.byetcluster.com', 'b13_32071822', '08bsvcwy', 'b_1332071822_razvalik');
